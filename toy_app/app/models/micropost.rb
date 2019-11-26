087b260b51e190ef4e1dd15112d320416b4c8eaf
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-11-26 20:38:30 +0900

    2.3.3 connect User model with Micropost's

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,4 +1,7 @@
 class Micropost < ApplicationRecord
+    # micropost投稿に対してuserは1対1
+    belongs_to :user
     # validationでcontents最大140文字の制約をつけた
-    validates :content, length: {maximum: 140}
+    validates :content, length: {maximum: 140},
+    presence: true
 end
--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    # user「1」に対してmicropostの投稿は多
+    has_many :microposts
+    validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
+    validates :email, presence: true    # 「FILL_IN」をコードに置き換えてください
 end
