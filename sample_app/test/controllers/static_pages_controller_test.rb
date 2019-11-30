c80a502daa4e5398cea4f0353fa45d9072426e9c
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-11-30 20:03:20 +0900

    3.4.3 演習 make test for contact page

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
+
+  # contactページを作成するにあたり、「Contact | Ruby on Rails Tutorial Sample App」というタイトルが存在するかどうかを確認する最初にtestをする
+  # その後にルーティングやviewの追加をしてテストをパスさせる
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
