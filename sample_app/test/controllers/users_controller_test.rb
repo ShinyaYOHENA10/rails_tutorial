9a3841a74ee67ae8bf9e569a20695f860ca4f23f
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-12-05 18:18:27 +0900

    5.4.2 update and test users/signup routing

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -2,8 +2,6 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-  # 演習 pathをsignupに変更してテスト
-  # 結果はRED
     get signup_path
     assert_response :success
   end
