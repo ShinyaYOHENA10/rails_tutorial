20d8030e5b170b91c7c9be2826dadac8389b5d0f
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-12-07 12:19:03 +0900

    6.2.2 test for validation

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+# setupメソッドは以下に続くテストより先に実行されるので、同メソッド内でインスタンス変数を定義、以降のtestメソッドで使用できるようにする
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
 end
