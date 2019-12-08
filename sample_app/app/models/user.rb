65d8a48e3935f82d815ed884241907fa47a0f5bb
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-12-08 17:08:23 +0900

    6.2.4 update user.rb

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     # ハッシュ最後尾に文字数上限追加
     validates :name, presence: true, length: {maximum: 50}
-    validates :email, presence: true, length: {maximum: 255}
+    # メールアドレス標準を定める公式サイトに完全な正規表現
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 255},
+    format: { with: VALID_EMAIL_REGEX }
 end
