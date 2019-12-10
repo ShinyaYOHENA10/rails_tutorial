3ed98856d803cf543acbc9b3b6a9f031143c8ec2
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-12-10 15:39:47 +0900

    7.3.4 演習

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -3,16 +3,17 @@ require 'test_helper'
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
   # 無効なユーザー登録に対するテスト
+  # assert_no_differenceメソッドのブロック内でpostを使い、メソッドの引数には'User.count'を与える
+  # assert_no_differenceのブロックを実行する前後で引数の値 (User.count) が変わらないことをテスト
   test "invalid signup information" do
     get signup_path
-    # assert_no_differenceメソッドのブロック内でpostを使い、メソッドの引数には'User.count'を与える
-    # assert_no_differenceのブロックを実行する前後で引数の値 (User.count) が変わらないことをテスト
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post signup_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'form[action="/signup"]'
   end
 end
