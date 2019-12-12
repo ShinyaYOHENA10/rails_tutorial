c449948e6dcf514a2c7f0721f543c7cfc237d021
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-12-12 18:18:26 +0900

    8.1.5 update flash message and fix routes

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -7,9 +7,12 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
   end
 
 end
