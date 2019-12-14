4731a8458cfaf87b1fc125149cd3f17c06ca80ed
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-12-14 15:15:09 +0900

    8.3 update logout function

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -3,7 +3,12 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-
+  
+   # 現在のユーザーをログアウトする
+   def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
 
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
