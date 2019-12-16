a14bd0758208ca072b2996dc60689c0f37f03cc8
ShinyaYOHENA10 <s.yohena10@gmail.com>
2019-12-16 23:54:32 +0900

    9.1.1 update users.rb

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,21 +1,33 @@
 class User < ApplicationRecord
-    #  email属性を小文字に変換してメールアドレスの一意性を保証
-    # before_save { self.email = email.downcase } 下の破壊的メソッドと同じ結果
-    before_save { email.downcase! }
-    # ハッシュ最後尾に文字数上限追加
-    validates :name, presence: true, length: {maximum: 50}
-    # メールアドレス標準を定める公式サイトに完全な正規表現
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: {maximum: 255},
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password, presence: true, length: {minimum: 6}
+  attr_accessor :remember_token
+  # email属性を小文字に変換してメールアドレスの一意性を保証
+  # before_save { self.email = email.downcase } 下の破壊的メソッドと同じ結果
+  before_save { email.downcase! }
+  # ハッシュ最後尾に文字数上限追加
+  validates :name, presence: true, length: {maximum: 50}
+  # メールアドレス標準を定める公式サイトに完全な正規表現
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+  format: { with: VALID_EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
 
-    # 渡された文字列のハッシュ値を返す
-    def User.digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                              BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す rails標準ライブラリ&メソッド
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 end
